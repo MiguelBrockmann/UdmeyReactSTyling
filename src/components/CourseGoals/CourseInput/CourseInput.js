535f7da618af22e479d4a6a4be75881ce68883d0
MiguelBrockmann <miguelbrockmann@gmail.com>
2022-05-27 15:32:58 +0200

    sasas

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,29 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid,setIsValid] = useState(true); // checken ob user  valider input
 
   const goalInputChangeHandler = event => { // was im inputfeldsteht steht nacher bei entered value
-    setEnteredValue(event.target.value);
+    if (event.target.value.trim().length > 0){ // ist valide soll also wieder true sein, damit farbchange nachher wieder geht
+        setIsValid(true);
+    }
+      setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length ===0){ //trim entfern whitespace am anfang  und ende, wollen mit diesem if nichts return wenn user einfach so submittet ohne text
+       setIsValid(false);
+        return;
+    }
     props.onAddGoal(enteredValue); // gibt info an app js, nach sub,mit
   };
 
   return (
     <form onSubmit={formSubmitHandler}>  {/*fkt wird aufgerufen wenn submitbutton gedrückt wird*/}
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} /> jedes mal wenn char eingegebn wird, wird fkt aufgerufen
+        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label> {/*jenachdem werlcher boolean isValid hat ändert sich farbe vom label*/}
+        <input style={{borderColor: !isValid ? 'red' : 'black', backgroundColor: !isValid ? 'salmon' : 'transparent'}} type="text" onChange={goalInputChangeHandler} /> {/*jedes mal wenn char eingegebn wird, wird fkt aufgerufen*/}
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
@@ -27,3 +35,14 @@ const CourseInput = props => {
 };
 
 export default CourseInput;
+
+/*
+alternative zu inlinestyle ist <div className=`form-control ${!isValid ? 'invalid' : ''} `}> falls   expresseion true hat formcontroll noch zusätzlich den invalid string was ihn dann eine andere css klasse gibt, kann auch mehr als 2 sein
+
+und dann in css
+
+.form-control.invalid {  alle classen die ein formcontroll haben und ein invalid
+  border-color: red;
+}
+
+*/
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,39 @@
 import React from 'react';
-
+import styled from "styled-components"; // ging nur weil wir vorher npm install --save styled-components gemacht haben
 import './Button.css';
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+
+//styled wurde importiert und styled kann butoon aufrufen, wird ein neuen button komponentn erschaffen,,, statt .button geht auch .p div usw ,,,,in ``kommen styles
+const Button = styled.button`     
+
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+ 
+
+&:focus {                   //wenn dieser button  focus hat mach diesen style
+  outline: none;
+}
+
+&:hover,
+&:active {
+  background: #ac0e77;
+  border-color: #ac0e77;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+}
+
+`;
+
+// const Button = props => { brauchen dass nicht mehr haben jetzt styled komponent
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
